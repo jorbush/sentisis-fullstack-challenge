2e4bc341f970d4214f99093580a04b58a8c61e02
Jordi <55429631+jorbush@users.noreply.github.com>
2024-10-10 08:37:20 +0200

    fix lint problems

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -5,7 +5,7 @@ export async function POST(request: Request) {
     const body = await request.json();
     const { title, type, releaseDate, description, price, currency } = body;
 
-    Object.keys(body).forEach((value: any) => {
+    Object.keys(body).forEach((value: keyof typeof body) => {
         if (!body[value]) {
             NextResponse.error();
         }
--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
+/* eslint-disable no-var */
 declare global {
     var prisma: PrismaClient | undefined;
 }
